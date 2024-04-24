a5d591f2989748302c751a4e4f08e74b1d12979a
Dipak Dahal <57629365+Dipak-108@users.noreply.github.com>
2024-04-24 11:48:45 +0545

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,61 @@
+let counter = 0;
+
+/*
+
+
+function decrease() {
+   
+     counter=counter-1;
+
+     document.getElementById("number_change").innerHTML=counter;
+    
+}
+
+function reset(){
+    counter=0;
+    document.getElementById("number_change").innerHTML=counter;
+}
+
+function increase(){
+counter++;
+document.getElementById("number_change").innerHTML=counter;
+}
+*/
+
+//console.log(document.querySelectorAll(".btn "));
+
+let btns = document.querySelectorAll(".btn");
+let value=document.querySelector("#number_change");
+
+//console.log(btns);
+
+btns.forEach((btn) => {
+ //console.log(btn);
+
+  btn.addEventListener("click", function (e) {
+
+   console.log(e.currentTarget);
+    const styles = e.currentTarget.classList;
+    //console.log(styles);
+
+    if (styles.contains("decrease")) {
+
+      counter--;
+
+      value.textContent=counter;
+
+      
+    }
+
+    else if(styles.contains("increase")){
+      counter++;
+      value.textContent=counter;
+    }
+
+
+    else{
+      counter=0;
+      value.textContent=counter;
+    }
+  });
+});
